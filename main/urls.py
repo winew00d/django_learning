8d101707724482db6834c61b7b59299c7126ee5d
pwd <90255464+winew00d@users.noreply.github.com>
2021-12-17 15:47:58 +0200

    update 17.12.2021

--- main/urls.py
+++ main/urls.py
@@ -3,6 +3,7 @@ from .import views
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
-    path('', views.index)
+    path('', views.index, name = 'home'),
+    path('about/', views.about, name = 'about')
     
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
